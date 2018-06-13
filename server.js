20b729714462969a1a419193390b8cdc792644a0
vishal <vishal.k.goyal90@gmail.com>
2018-06-13 13:09:52 +0530

    Setup Start script

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -33,4 +36,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(port, ()=>{
+  console.log(`Server is ruuning on port : ${port}`);
+});
